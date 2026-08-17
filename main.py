9f85b11536a26906e6442096740a6b90b8e0008a
mori <BogdanKiryuchshenko@gmail.com>
2026-08-18 02:20:15 +0500

    moving to a new laptop

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 
 import pygame
+import sys
+from player import Player 
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from logger import log_event
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print (f"Screen width: {SCREEN_WIDTH}") 
     print (f"Screen height: {SCREEN_HEIGHT}")
-
     while True:
         log_state()
         for event in pygame.event.get():
-         pass
+         if event.type == pygame.QUIT:
+            return
         screen.fill((0, 0, 0))
+        updatable.update(dt)
+
+        for sprites in asteroids:
+            if sprites.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            
+
+        for draws in drawable:
+            draws.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
-
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
-
 
---
+++ shot.py
@@ -0,0 +1,11 @@
+import constants
+
+class Shot(CircleShape):
+    def __init__(self,x,y):
+        super().__init__(x,y,SHOT_RADIUS)
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS)
+
+    def update(self, dt: float):
+        self.position += self.velocity * dt
